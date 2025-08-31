a4803d34e25f42d684617d558b26d7cdad12d792
Anupam Mondal <a09051985@gmail.com>
2025-09-01 02:15:56 +0530

    Add dummy data object in dummyData.js

---
+++ utils/dummyData.js
@@ -0,0 +1,8 @@
+const DummyData = {
+  name: "Sample Name",
+  description: "Sample description text",
+  url: "https://example.com",
+  image: "https://example.com/image.jpg",
+  phone: "+1-000-000-0000",
+  address: "123 Sample Street, City, Country"
+};
